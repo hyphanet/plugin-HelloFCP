418617eff7f7de0d96056884f7513daf9f5224b1
saces <saces@freenetproject.org>
2007-11-17 23:09:20 +0000

    update HelloFCP plugin (new flag) doc
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/HelloFCP@15809 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/HelloFCP/HelloFCP.java
+++ src/plugins/HelloFCP/HelloFCP.java
@@ -24,7 +24,21 @@ public class HelloFCP implements FredPlugin, FredPluginThreadless, FredPluginFCP
 		
 	}
 
-	public FCPMessage create(String name, SimpleFieldSet fs) {
+	/**
+	 * sample command:
+	 * <pre>
+	 * plugins.HelloFCP.HelloFCP.Ping
+	 * EndMessage
+	 * </pre>
+	 * the node have found our class (plugins.HelloFCP.HelloFCP) and want now to
+	 * know what to do with "Ping"
+	 * 
+	 * the fullaccess flag is set true if the client comes from an adress with full fcp access,
+	 * otherwise false
+	 * 
+	 * @see freenet.pluginmanager.FredPluginFCP#create(java.lang.String, freenet.support.SimpleFieldSet, boolean)
+	 */
+	public FCPMessage create(String name, SimpleFieldSet fs, boolean fullaccess) {
 		if(name.equals(PingMessage.name))
 			return new PingMessage(fs);
 		
