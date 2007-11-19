865e9f5481f0452cb56e17d0d7b63bc5e2a57691
saces <saces@freenetproject.org>
2007-11-19 21:49:17 +0000

    update HelloFCP plugin
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/HelloFCP@15859 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/HelloFCP/HelloFCP.java
+++ src/plugins/HelloFCP/HelloFCP.java
@@ -1,14 +1,15 @@
-/**
- * 
- */
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package plugins.HelloFCP;
 
-import freenet.node.fcp.FCPMessage;
+import freenet.pluginmanager.FCPPluginOutputWrapper;
 import freenet.pluginmanager.FredPlugin;
 import freenet.pluginmanager.FredPluginFCP;
 import freenet.pluginmanager.FredPluginThreadless;
 import freenet.pluginmanager.PluginRespirator;
 import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
 
 /**
  * @author saces
@@ -23,26 +24,17 @@ public class HelloFCP implements FredPlugin, FredPluginThreadless, FredPluginFCP
 	public void terminate() {
 		
 	}
-
+	
 	/**
-	 * sample command:
-	 * <pre>
-	 * plugins.HelloFCP.HelloFCP.Ping
-	 * EndMessage
-	 * </pre>
-	 * the node have found our class (plugins.HelloFCP.HelloFCP) and want now to
-	 * know what to do with "Ping"
 	 * 
 	 * the fullaccess flag is set true if the client comes from an adress with full fcp access,
 	 * otherwise false
 	 * 
 	 * @see freenet.pluginmanager.FredPluginFCP#create(java.lang.String, freenet.support.SimpleFieldSet, boolean)
 	 */
-	public FCPMessage create(String name, SimpleFieldSet fs, boolean fullaccess) {
-		if(name.equals(PingMessage.name))
-			return new PingMessage(fs);
-		
-		return null;
+	public void handle(FCPPluginOutputWrapper replysender, SimpleFieldSet params, Bucket data, boolean fullacess) {
+		// simple echo
+		replysender.send(params);
 	}
 
 }
